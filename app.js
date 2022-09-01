2957318771a272faa64e919ceb6801d36f1c65d0
Elias <elias.staehle@gmail.com>
2022-08-31 21:29:55 -0400

    added js and fixed css

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+// let message;
+// let usersName = prompt('what is your name?');
+// message;
+// if (usersName == 'Elias') {
+//     message = 'hello there';
+// }
+// else {
+//     let stranger = prompt('hi ' + usersName + ' do you like to travel?');
+//     if (stranger != 'yes') {
+//         message = 'why are you here?';
+//     }
+//     else {
+//         message = "welcome traveler";
+//     }
+
+// }
+
+// document.write(message);
+
